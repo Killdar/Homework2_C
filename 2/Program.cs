03b284169c0ae0dc98fb7372a62062d39687c70b
Killdar <mykolahorobii@gmail.com>
2023-07-27 12:52:40 +0200

    homework2

---
+++ 2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int thirdDigit = GetThirdDigit(number);
+
+if (thirdDigit == -1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Третья цифра числа: " + thirdDigit);
+}
+
+
+static int GetThirdDigit(int number)
+{
+    if (number < 100)
+    {
+        // Число меньше трехзначного
+        return -1;
+    }
+
+    int thirdDigit = (number / 100) % 10;
+    return thirdDigit;
+}
+
+
+
